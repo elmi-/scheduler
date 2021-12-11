9e743496852cf77292387ae560c3e1214334e977
Abdi <abdi.elmi@rocketmail.com>
2021-12-10 16:54:19 -0800

    added schedule to application and removed hard-coded days and replaced with api request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,32 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
-import Header from "./Appointment/Header";
-import Show from "./Appointment/Show";
-import Empty from "./Appointment/Empty";
-
+import Appointment from "./Appointment"; 
+import appointments from "sampleData";
 import "components/Application.scss";
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-  
+  const [days, setDays] = useState([]);  
   const [day, setDay] = useState("Monday");
+
+  const parsedAppointments = appointments.map(appointment => 
+    <Appointment 
+      key={appointment.id}
+      {...appointment}  
+    />);
+
+  useEffect(() => {
+    const daysURL = "http://localhost:8001/api/days";
+    axios.get(daysURL)
+      .then(response => {
+        // console.log(response.data);
+        setDays(response.data);
+      })
+      .catch(err => {
+        console.log(err.message)
+      });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -45,7 +46,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        { parsedAppointments }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
