b38bf8a4f17e38eb9f75b38a5c1af3c1ace4bd51
Abdi <abdi.elmi@rocketmail.com>
2021-12-14 22:02:17 -0800

    type checking with prop-types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
-
 import "components/InterviewerList.scss"
+
+
 // InterviewerList 
-export default function InterviewerList(props) {
+ function InterviewerList(props) {
   const interviewers = props.interviewers;
   const parsedInterviewerList = interviewers.map(interviewer => 
     <InterviewerListItem 
@@ -23,3 +25,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import appointments from "sampleData";
 
 export default function useApplicationData () {
     const [state, setState] = useState({
