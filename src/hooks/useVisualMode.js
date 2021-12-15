24036ddef9eed5a0dc15678cc9bb82bc7c1be236
Abdi <abdi.elmi@rocketmail.com>
2021-12-15 14:26:37 -0800

    fixed back() with help from mentor

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,33 +4,27 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   
-  function transition(newMode, replace = false) {
+  function transition(mode, replace = false) {
     const newHistory = [...history];
+
     if(replace) {
       newHistory.pop();
     }
 
-    newHistory.push(newMode);
+    newHistory.push(mode);
+    setMode(mode);
     setHistory(newHistory);
-    setMode(newMode);
   }
 
   function back() {
     const newHistory = [...history];
 
-    if (newHistory.length < 2) {
-      return newHistory[0];
-    }
-
-    let popVal = newHistory.pop();
-
-    if (popVal === mode) {
-      popVal = newHistory.pop();
-    }
-    
-    setHistory(newHistory);
-    setMode(popVal);
+    if (newHistory.length > 1) {
+      newHistory.pop();
+      setMode(newHistory[newHistory.length - 1]);
+      setHistory([...newHistory]);
   }
+}
 
   return { mode, transition, back };
 }
