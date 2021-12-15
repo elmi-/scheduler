d251e942772e85e3fae888d145eba7c0b73ce9b3
Abdi <abdi.elmi@rocketmail.com>
2021-12-14 19:57:07 -0800

    updated spots remaining (help with mentor

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import appointments from "sampleData";
 
 export default function useApplicationData () {
     const [state, setState] = useState({
       day: "Monday",
       days: [],
-      appointments: {}
+      appointments: {},
+      // spots: 0
     });
     
     useEffect(() => {
@@ -35,13 +37,27 @@ export default function useApplicationData () {
     const setDays = days =>   setState(prev => ({ ...prev, days }));
     
     const cancelAppointment = (id) => {
-      console.log(id);
-      return axios.delete(`http://localhost:8001/api/appointments/${ id }`)
+      const appointment = {
+        ...state.appointments[id],
+        interview: null,
+      };
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment,
+      };
+   
+      const cDay = state.days.find(day => day.appointments.includes(id))
+      const nDay = {...cDay, spots: cDay.spots+1}
+      const nDays =  state.days.map((day) => {return day.name === state.day ? nDay: day})
+        
+      return axios.delete(`http://localhost:8001/api/appointments/${id}`)
       .then((res) => {
-        // setState({
-        //   ...state,
-        //   appointments
-        // });
+        // console.log("setting the new state...")
+           setState({
+          ...state,
+          appointments,
+          days: nDays
+        });
       })
       .catch((err) => {
         console.log(err.message);
@@ -58,12 +74,17 @@ export default function useApplicationData () {
         ...state.appointments,
         [id]: appointment
       };
-    
+
+      const cDay = state.days.find(day => day.appointments.includes(id))
+      const nDay = {...cDay, spots: cDay.spots-1}
+      const nDays =  state.days.map((day) => {return day.name === state.day ? nDay: day})
+
       return axios.put(`http://localhost:8001/api/appointments/${ id }`, { interview })
       .then((res) => {
         setState({
           ...state,
-          appointments
+          appointments,
+          days: nDays
         });
       })
       .catch((err) => {
